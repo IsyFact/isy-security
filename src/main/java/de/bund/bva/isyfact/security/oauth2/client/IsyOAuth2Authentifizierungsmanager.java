90d01149e95c2fe24db6ec11e6e892ebd733aaa3
Fabian Perl <fabian.perl@seitenbau.com>
2025-09-25 13:41:11 +0200

    fix: IFS-4871 nullpointer warning without Assert statement

--- src/main/java/de/bund/bva/isyfact/security/oauth2/client/IsyOAuth2Authentifizierungsmanager.java
+++ src/main/java/de/bund/bva/isyfact/security/oauth2/client/IsyOAuth2Authentifizierungsmanager.java
@@ -179,7 +179,10 @@ public class IsyOAuth2Authentifizierungsmanager implements Authentifizierungsman
         if (clientRegistrationRepository != null) {
             clientRegistration = clientRegistrationRepository.findByRegistrationId(oauth2ClientRegistrationId);
         }
-        Assert.notNull(clientRegistration, "Could not find ClientRegistration with id '" + oauth2ClientRegistrationId + "'");
+
+        if (clientRegistration == null) {
+            throw new IllegalArgumentException("Could not find ClientRegistration with id '%s'".formatted(oauth2ClientRegistrationId));
+        }
 
         // load additional props for this registration ID, can be null
         AdditionalRegistrationProperties props = isyOAuth2ClientProps.getRegistration().get(clientRegistration.getRegistrationId());
@@ -224,7 +227,10 @@ public class IsyOAuth2Authentifizierungsmanager implements Authentifizierungsman
         if (clientRegistrationRepository != null) {
             clientRegistration = clientRegistrationRepository.findByRegistrationId(oauth2ClientRegistrationId);
         }
-        Assert.notNull(clientRegistration, "Could not find ClientRegistration with id '" + oauth2ClientRegistrationId + "'");
+
+        if (clientRegistration == null) {
+            throw new IllegalArgumentException("Could not find ClientRegistration with id '%s'".formatted(oauth2ClientRegistrationId));
+        }
 
         AuthorizationGrantType grantType = clientRegistration.getAuthorizationGrantType();
 
