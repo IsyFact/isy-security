63abc32e4c0f8ee6d791c9a15f96c5940f8ca41e
Martin Glörfeld <martin.gloerfeld@materna.group>
2026-08-19 17:15:11 +0200

    feat: IFS-5745 restore and rebuild parts of the ROPC flow

---
+++ isy-security/src/main/java/de/bund/bva/isyfact/security/oauth2/client/authentication/token/PasswordClientRegistrationAuthenticationToken.java
@@ -0,0 +1,72 @@
+package de.bund.bva.isyfact.security.oauth2.client.authentication.token;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.lang.Nullable;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.util.SerializationUtils;
+
+/**
+ * AuthenticationToken holding parameters required for creating a Client to use with Resource Owner Password Credentials Flow authentication.
+ */
+public class PasswordClientRegistrationAuthenticationToken extends AbstractClientRegistrationAuthenticationToken {
+
+    /** The resource owner's username. */
+    private final String username;
+
+    /** The resource owner's password. */
+    private final String password;
+
+    public PasswordClientRegistrationAuthenticationToken(ClientRegistration clientRegistration, String username, String password, @Nullable String bhknz) {
+        super(username, clientRegistration, bhknz);
+        this.username = username;
+        this.password = password;
+        setAuthenticated(false);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Generates a cache key that includes the following fields.
+     * <ul>
+     *     <li>principal</li>
+     *     <li>bhknz</li>
+     *     <li>issuerLocation</li>
+     *     <li>clientId</li>
+     *     <li>clientSecret</li>
+     *     <li>authorizationGrantType</li>
+     *     <li>username</li>
+     *     <li>password</li>
+     * </ul>
+     *
+     * @return the generated cache key as hash code or null
+     */
+    @Override
+    public byte[] generateCacheKey(String hashAlgorithm, byte[] salt) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance(hashAlgorithm);
+
+            digest.update(super.generateCacheKey(hashAlgorithm, salt));
+
+            List<String> additionalValues = Arrays.asList(
+                String.valueOf(getUsername()),
+                String.valueOf(getPassword())
+            );
+            byte[] additionalBytes = SerializationUtils.serialize(additionalValues);
+            digest.update(additionalBytes);
+
+            return digest.digest();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(hashAlgorithm + " nicht verfügbar.", e);
+        }
+    }
+}
